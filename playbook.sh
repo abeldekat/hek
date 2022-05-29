c6cdb4eff76f9848af9c9f08368c21b7d6d8cbf6
abeldekat <abel@nomail.com>
2022-05-29 17:25:15 +0200

    first steps for searxng

--- playbook.sh
+++ playbook.sh
@@ -4,24 +4,14 @@
 # --> pip install netaddr
 
 # ansible [core 2.12.5]
-#   config file = /home/chris/git/github/algo/ansible.cfg
-#   configured module search path = ['/home/chris/.ansible/plugins/modules', '/usr/share/ansible/plugins/modules']
+#   config file = /home/user/git/github/algo/ansible.cfg
+#   configured module search path = ['/home/user/.ansible/plugins/modules', '/usr/share/ansible/plugins/modules']
 #   ansible python module location = /usr/lib/python3.10/site-packages/ansible
-#   ansible collection location = /home/chris/.ansible/collections:/usr/share/ansible/collections
+#   ansible collection location = /home/user/.ansible/collections:/usr/share/ansible/collections
 #   executable location = /usr/bin/ansible
 #   python version = 3.10.4 (main, Mar 23 2022, 23:05:40) [GCC 11.2.0]
 #   jinja version = 3.0.3
 #   libyaml = True
 
-# algo: ansible-core==2.12.3
-# matrix-ansible: We generally support Ansible 2.7.1 and above.
-# no need for:
-# provider=digitalocean
-# ondemand_cellular=false
-# ondemand_wifi=false
-ansible-playbook -K playbook.yml -e "server_name=algo
-                                dns_adblocking=true
-                                ssh_tunneling=true
-                                store_pki=true
-                                region=ams3
-                                do_token=token"
+# ansible-playbook -K playbook.yml --tags="searxng"
+ansible-playbook -K playbook.yml
