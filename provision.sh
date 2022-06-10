3b532c156e445c3952f88a705c5726b85b3ef78d
abeldekat <abel@nomail.com>
2022-06-10 20:27:14 +0200

    Refactor inventory and default vars in roles. Wireguard

--- provision.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-# ipv6: true digital ocean yml -> when creating a droplet, also activate ipv6
-# reference on server: /etc/network/interfaces/cloud_init
-#
-ansible-playbook -e "ansible_ssh_user=root" provision.yml 
